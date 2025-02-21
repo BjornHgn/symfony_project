e648212b516fd2af9dc5c2e2804513277ac23131
Vincenzo <vincent.luu@ynov.com>
2025-02-21 11:17:42 +0100

    create /profile for change personal information

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    public function profile(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = $this->getUser();
+
+        if ($request->isMethod('POST')) {
+            $user->setUsername($request->request->get('username'));
+            $user->setEmail($request->request->get('email'));
+
+            if ($request->files->get('profil_picture')) {
+                $file = $request->files->get('profil_picture');
+                $filename = md5(uniqid()) . '.' . $file->guessExtension();
+                $file->move($this->getParameter('images_directory'), $filename);
+                $user->setProfilPicture($filename);
+            }
+
+            if ($request->request->get('current_password') && $request->request->get('new_password') && $request->request->get('confirm_password')) {
+                if ($passwordHasher->isPasswordValid($user, $request->request->get('current_password'))) {
+                    if ($request->request->get('new_password') === $request->request->get('confirm_password')) {
+                        $user->setPassword(
+                            $passwordHasher->hashPassword($user, $request->request->get('new_password'))
+                        );
+                    } else {
+                        $this->addFlash('error', 'Les nouveaux mots de passe ne correspondent pas.');
+                    }
+                } else {
+                    $this->addFlash('error', 'Le mot de passe actuel est incorrect.');
+                }
+            }
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Profil mis à jour avec succès.');
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('home/profile.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
