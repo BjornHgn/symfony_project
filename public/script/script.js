3c46313a41c28351f90cc8d878804e139982e564
plionel <lionel.payet@ynov.com>
2025-02-28 10:04:31 +0100

    rangement

---
+++ public/script/script.js
@@ -0,0 +1,9 @@
+
+function toggleSearchField() {
+    var searchForm = document.getElementById('searchForm');
+    if (searchForm.style.display === 'none') {
+        searchForm.style.display = 'block';
+    } else {
+        searchForm.style.display = 'none';
+    }
+}
